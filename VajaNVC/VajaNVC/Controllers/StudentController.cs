506d7fef8bb706f335798646484eed92398a1568
AleksM <aleks.mavric123@gmail.com>
2018-04-16 10:14:24 +0200

    updejt

---
+++ VajaNVC/VajaNVC/Controllers/StudentController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VajaNVC.Models;
+
+namespace VajaNVC.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            var studentList = new List<student>
+            {
+                new student() {StudentId=1, StudentName="John", Age=18 },
+                new student() {StudentId=1, StudentName="Janez", Age=18 },
+                new student() {StudentId=1, StudentName="Marija", Age=19 },
+                new student() {StudentId=1, StudentName="Toni", Age=20 },
+                new student() {StudentId=1, StudentName="Franci", Age=22 }
+
+            };
+
+            return View(studentList);
+        }
+    }
+}
---
+++ VajaNVC/VajaNVC/Models/student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace VajaNVC.Models
+{
+    public class student
+    {
+        public int StudentId { get; set; }
+        [Display(Name="Ime")]
+        public string StudentName { get; set; }
+        [Display(Name = "Starost")]
+        public int Age { get; set; }
+    }
+}
